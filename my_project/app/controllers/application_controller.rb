63a9a12d0abcb24091e75cd4ee925d71837ebeb7
Gabriel Carvalho <nihongabriel@gmail.com>
2014-11-05 16:15:35 -0200

    Creating route to my application.
    Creating simple methods to my application.

--- my_project/app/controllers/application_controller.rb
+++ my_project/app/controllers/application_controller.rb
@@ -2,4 +2,31 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def hello
+    render text: "Hello MODAFOKASSSS!!!!"
+  end
+
+#Argumentos splat são argumentos precedidos por *, que sinaliza: "Ei Ruby, Eu não sei quantos argumentos serão passados, mas pode ser mais de um."
+  def what_up(greeting, *bros)
+    bros.each { |bro| puts "#{greeting}, #{bro}!" }
+  end
+
+#This method organizes in reverse mode, sort!
+  def alphabetize (arr, rev=false)
+    if rev==true
+      arr.reverse!
+#{|firstItem, secondItem| secondItem <=> firstItem}
+    else
+      rev==false
+      arr.sort!
+    end
+  end
+
+#This method returns each value of an array * its own value
+  def multiply_element(array)
+    array.each do |x|
+      x=x*x
+    end
+  end
 end
